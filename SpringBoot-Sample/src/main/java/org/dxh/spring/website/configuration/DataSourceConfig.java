850859c85e235e159b40ee809e07fcc2d36f1bb4
xiaohujapan <xiaohujapan@yahoo.co.jp>
2019-03-13 11:53:47 +0900

    ソースMerge

--- SpringBoot-Sample/src/main/java/org/dxh/spring/website/configuration/DataSourceConfig.java
+++ SpringBoot-Sample/src/main/java/org/dxh/spring/website/configuration/DataSourceConfig.java
@@ -21,22 +21,31 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
+@MapperScan(basePackages = "org.dxh.spring.website.mapper")
 @Configuration
 public class DataSourceConfig {
+	
 	@Bean(name = "db_master")
 	@ConfigurationProperties(prefix = "multiple.datasource.master") 
-	public DataSource dataSource1() {
-		log.info("Master-DB");
-		return DataSourceBuilder.create().build();
+	public DataSource dataSourceMaster() {
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
 	@Bean(name = "db_slave")
 	@ConfigurationProperties(prefix = "multiple.datasource.slave") 
-	public DataSource dataSource2() {
-		log.info("Slave-DB");
+	public DataSource dataSourceSlave() {
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
+	}
+	
+	@Bean(name = "db_mail")
+	@ConfigurationProperties(prefix = "multiple.datasource.mail") 
+	public DataSource dataSourceMail() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean(name = "db_access")
+	@ConfigurationProperties(prefix = "multiple.datasource.access") 
+	public DataSource dataSourceAccess() {
 		return DataSourceBuilder.create().build();
 	}
 	
@@ -44,13 +53,39 @@ public class DataSourceConfig {
 	@Primary
 	public DataSource dataSource() {
 		DynamicDataSource dynamicDataSource = new DynamicDataSource();
-		DataSource master = dataSource1();
-		DataSource slave = dataSource2();
+		DataSource master 	  = dataSourceMaster();
+		DataSource slave   	  = dataSourceSlave();
+		//DataSource mail   	  = dataSourceMail();
+		//DataSource access    = dataSourceAccess();
+		
 		dynamicDataSource.setDefaultTargetDataSource(master);	
+		
 		Map<Object,Object> map = new HashMap<>();
 		map.put(DataSourceType.DB_Master.getName(), master);
 		map.put(DataSourceType.DB_Slave.getName(), slave);
+		//map.put(DataSourceType.DB_MAIL.getName(), slave);
+ 	    //map.put(DataSourceType.DB_ACCESS.getName(), slave);
+		
 		dynamicDataSource.setTargetDataSources(map);			
 		return dynamicDataSource;
 	}
+	
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+	
 }
